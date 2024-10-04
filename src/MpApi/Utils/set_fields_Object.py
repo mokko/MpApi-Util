4e754c2fcb2a00487d3d4cbd6127cd20006c10ff
Maurice Mengel <mauricemengel@gmail.com>
2024-10-04 20:16:05 +0200

    becky.py: erwerbungsart and -datum working

--- src/MpApi/Utils/set_fields_Object.py
+++ src/MpApi/Utils/set_fields_Object.py
@@ -27,6 +27,23 @@ roles = {
     "Vorbesitzer*in": 4378466,
 }
 
+erwerbungsarten = {
+    "Auktion": 1630981,
+    "Beschlagnahmung": 1630982,
+    "Kauf": 1630987,
+    "Kommission": 1630988,
+    "Leihe": 1630989,
+    "Nachlass/Vermächtnis": 1630990,
+    "Pfändung": 1630992,
+    "Restitution": 1630993,
+    "Rückgewinnung": 2737042,
+    "Schenkung": 1630994,
+    "Übereignung": 4129997,
+    "Überweisung": 1630996,
+    "Zugang ungeklärt": 1631000,
+    "Zugang ungeklärt (Expedition)": 1631001,
+}
+
 
 def set_beteiligte(recordM: Module, *, beteiligte: str, conf: dict) -> None:
     """
@@ -120,22 +137,99 @@ def set_ident_sort(record: Module, *, nr: int) -> None:
     )
 
 
-def set_erwerbDatum(recordM: Module, *, datum: str) -> None: ...
+def set_erwerbDatum(recordM: Module, *, datum: str) -> None:
+    """
+    ObjAcquisitionDateGrp
+
+    N.B. Whe I requested to use dataField[@name = 'ObjAcquisitionDateGrp'], RIA didnn't
+    delete old entries from the template.
+    """
+    print(f"Erwerb.datum={datum}")
+    quelle = "Hauptkatalog / Kamerun 2024"
+    newN = etree.fromstring(f"""
+        <repeatableGroup name="ObjAcquisitionDateGrp">
+          <repeatableGroupItem>
+            <dataField name="DateToTxt">
+              <value>{datum}</value>
+            </dataField>
+            <dataField name="SourceTxt">
+              <value>{quelle}</value>
+            </dataField>
+            <dataField name="SortLnu">
+              <value>1</value>
+            </dataField>
+            <virtualField name="PreviewVrt">
+              <value>{datum}</value>
+            </virtualField>
+          </repeatableGroupItem>
+        </repeatableGroup>
+    """)
+    _new_or_replace(
+        record=recordM,
+        xpath="//m:repeatableGroup[@name = 'ObjAcquisitionDateGrp']",
+        newN=newN,
+    )
 
 
-def set_erwerbNr(recordM: Module, *, nr: str) -> None: ...
+def set_erwerbNr(recordM: Module, *, nr: str) -> None:
+    """
+    TODO
+    """
+    print(f"{nr=}")
 
 
-def set_erwerbungsart(recordM: Module, *, art: str) -> None: ...
+def set_erwerbungsart(recordM: Module, *, art: str) -> None:
+    """
+    ObjAcquisitionMethodGrp
+
+    instanceName="ObjAcquisitionMethodVgr"
+    """
+    global erwerbungsarten
+    try:
+        artID = erwerbungsarten[art]
+    except IndexError:
+        raise IndexError(f"Erwerbungsart unbekannt: '{art}'")
+    print(f"Erwerbungsart={art} {artID=}")
+    bemerkung = "Kamerun 2024"
+
+    newN = etree.fromstring(f"""
+        <repeatableGroup name="ObjAcquisitionMethodGrp">
+          <repeatableGroupItem>
+            <dataField name="NotesClb">
+              <value>{bemerkung}</value>
+            </dataField>
+          <vocabularyReference name="MethodVoc" id="62639" >
+            <vocabularyReferenceItem id="{artID}"/>
+          </vocabularyReference>
+          </repeatableGroupItem>
+        </repeatableGroup>
+    """)
+    _new_or_replace(
+        record=recordM,
+        xpath="//m:repeatableGroup[@name = 'ObjAcquisitionMethodGrp']",
+        newN=newN,
+    )
 
 
-def set_erwerbVon(recordM: Module, *, von: str) -> None: ...
+def set_erwerbVon(recordM: Module, *, von: str) -> None:
+    """
+    TODO
+    """
+    print(f"{von=}")
 
 
-def set_geogrBezug(recordM: Module, *, name: str) -> None: ...
+def set_geogrBezug(recordM: Module, *, name: str) -> None:
+    """
+    TODO
+    """
+    print(f"geogrBezug {name=}")
 
 
-def set_objRefA(recordM: Module, *, keineAhnung: str) -> None: ...
+def set_objRefA(recordM: Module, *, keineAhnung: str) -> None:
+    """
+    TODO
+    """
+    print(f"objRefA {keineAhnung=}")
 
 
 def set_sachbegriff(record: Module, *, sachbegriff: str) -> None:
