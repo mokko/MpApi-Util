aba806509128c7f2565ec53babc3beec02bf1e9b
Maurice Mengel <mauricemengel@gmail.com>
2026-07-18 15:29:35 +0200

    becky switches back to record_exists for basic identity test to exclude Leipzig Rückführung.

--- src/MpApi/Utils/becky/becky.py
+++ src/MpApi/Utils/becky/becky.py
@@ -30,7 +30,7 @@ from mpapi.constants import get_credentials
 from mpapi.module import Module
 from mpapi.search import Search
 
-from MpApi.Utils.Ria import RIA, init_ria, record_exists2, record_exists3
+from MpApi.Utils.Ria import RIA, init_ria, record_exists, record_exists3
 from MpApi.Utils.becky.set_fields_Object import (
     set_ident,
     set_ident_sort,
@@ -195,7 +195,7 @@ def per_row(*, idx: int, row: Cell, conf: dict, act: bool) -> None:
         # record_exists2 is Hendryk's algorithm that uses schemata and fortlaufende Nummer
         # if m := record_exists2(ident=ident, conf=conf):
         # record_exists3 omits Bereich and simply uses IdentNr and exact match.
-        if m := record_exists3(ident=ident, conf=conf):
+        if m := record_exists(ident=ident, conf=conf):
             # Wollen wir hier Fehler loggen um Nachzuvollziehen, wo die Infos aus Excel
             # nicht eingetragen wurden? Nein. Nur loggen, wenn etwas in RIA verändert wird
             print(f"INFO Record '{ident}' exists already")
